ec4c39fc70bbcba3e15cadc0a907fe7bb007f131
MyMetric - Marketing Analytics <36673756+mymetric@users.noreply.github.com>
2025-02-17 14:02:38 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -80,7 +80,7 @@ function createPopup(imgUrl, titleText, subtitleText, postUrl, buttonText, close
 
   // Verifica se a URL contém "crm", "email" ou "mautic"
   const currentUrl = window.location.href.toLowerCase();
-  if (currentUrl.includes('crm') || currentUrl.includes('email') || currentUrl.includes('mautic')) {
+  if (currentUrl.includes('crm') || currentUrl.includes('email') || currentUrl.includes('mautic') || currentUrl.includes('whatsapp')) {
     setCookie('popup_closed', 'true', 7);
     console.log('Popup fechado automaticamente devido à URL contendo termos específicos.');
     return;
