8ed1c7aea62dbe8ed9d96d6557d4bb2290d96d79
MyMetric - Marketing Analytics <36673756+mymetric@users.noreply.github.com>
2025-02-17 09:55:36 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -37,6 +37,45 @@ function isMetaBrowser() {
   return isInstagram || isFacebook;
 }
 
+function getCookie(name) {
+  var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  if (match) {
+    return match[2];
+  } else {
+    return null;
+  }
+}
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function deleteCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999; path=/';
+}
+
+function isMobileDevice() {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+function isDebugMode() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.has('debug') && urlParams.get('debug') === 'true';
+}
+
+function isMetaBrowser() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  var isInstagram = userAgent.includes('Instagram');
+  var isFacebook = userAgent.includes('FBAN') || userAgent.includes('FBAV');
+  return isInstagram || isFacebook;
+}
+
 function createPopup(imgUrl, titleText, subtitleText, postUrl, buttonText, closeText, afterMessage, gtmPreviewCode, closeDays, buttonColor, buttonBgColor, disablePhoneField) {
 
   // Verifica se a URL contém "crm", "email" ou "mautic"
