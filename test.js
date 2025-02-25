6ec34d30a7cf33e95c36d0673d9e44561284687d
MyMetric - Marketing Analytics <36673756+mymetric@users.noreply.github.com>
2025-02-25 05:57:49 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+
+function mymetric_log(content) {
+    var mmBadge = 'MyMetric Pixel for Shopify';
+    var style1 = "background: #8430ce; color: white; padding: 1px 3px; border-radius: 1px; margin-right: 10px;";
+    var style2 = "color: white; font-weight: bold;";
+    console.log(`%c${mmBadge}%c${content}`, style1, style2);
+}
