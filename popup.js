690b1580ab3b959f1d4ae1834e3b6368dff84c1c
MyMetric - Marketing Analytics <36673756+mymetric@users.noreply.github.com>
2025-08-12 13:22:30 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+// v 1.0
+
 function getCookie(name) {
   var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
   if (match) {
@@ -283,13 +285,13 @@ function createPopup(
     var errors = [];
 
     if (!name) {
-      errors.push('Nome Ã© obrigatÃ³rio.');
+      errors.push('Nome obrigatório.');
     }
 
     if (!email) {
-      errors.push('Email Ã© obrigatÃ³rio.');
+      errors.push('Email obrigatório.');
     } else if (!validateEmail(email)) {
-      errors.push('Email invÃ¡lido.');
+      errors.push('Email inválido.');
     }
 
     if (errors.length > 0) {
